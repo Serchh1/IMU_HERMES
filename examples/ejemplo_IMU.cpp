409591c94a5f58fd4bc8d89f57b07d06930e666d
Sergio Murillo <smurillo@novaera.edu.mx>
2024-08-28 12:37:14 -0600

    We agree debug flags to be parctical

--- examples/ejemplo_IMU.cpp
+++ examples/ejemplo_IMU.cpp
@@ -1,26 +1,22 @@
-// #include <Arduino.h>
-// #include <IMU.h>
+#include <Arduino.h>
+#include <IMU.h>
 
-// IMU_hermes imu;
+IMU_hermes myIMU;
 
-// void setup()
-// {
-//     imu.begin();
-// }
+void setup()
+{
+    myIMU.begin();
+}
 
-// void loop()
-// {
-//     imu.update(10);
-    
-//     Serial.print(imu.quatI, 2);
-//     Serial.print(F(","));
-//     Serial.print(imu.quatJ, 2);
-//     Serial.print(F(","));
-//     Serial.print(imu.quatK, 2);
-//     Serial.print(F(","));
-//     Serial.print(imu.quatReal, 2);
-//     Serial.print(F(","));
-//     Serial.print(imu.quatRadianAccuracy, 2);
-//     Serial.print(F(","));
-//     Serial.println();
-// }
+void loop()
+{   
+    Serial.print(myIMU.quatI, 2);
+    Serial.print(F(","));
+    Serial.print(myIMU.quatJ, 2);
+    Serial.print(F(","));
+    Serial.print(myIMU.quatK, 2);
+    Serial.print(F(","));
+    Serial.print(myIMU.quatReal, 2);
+    Serial.println();
+    delay(500);
+}
